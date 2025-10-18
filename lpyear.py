524a8318f62693d6f00f22c7683f4e855d8f5a01
Zaidzhk10 <zaidzhk10@gmail.com>
2025-10-18 13:20:32 +0530

    second

--- lpyear.py
+++ lpyear.py
@@ -1,6 +1,14 @@
-year=int(input("Enter the year:"))
-if (year % 4 == 0):
-    print("it is a leap year")
+def is_leap(year):
+   
+    if (year % 4 == 0):
+        return True
+    return False
 
-else:
-    print("it is not a leap year")
+def check_leap_year(years):
+    leap_years = [year for year in years if is_leap(year)]
+    return leap_years
+
+
+years = list(map(int, input("Enter the years : ").split()))
+leap_years = check_leap_year(years)
+print(f"Leap years from the list are: {leap_years}")
